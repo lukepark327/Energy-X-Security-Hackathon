2c7c787fd80c8db4890f2c2fcdd4bebc788305f9
jmlee <jmlee@altair.snu.ac.kr>
2019-10-15 16:48:43 +0900

    add comments

--- uniswap/contract/UniswapExchange.sol
+++ uniswap/contract/UniswapExchange.sol
@@ -5,6 +5,7 @@ import "./ERC20Interface.sol";
 import "./UniswapFactory.sol";
 
 
+// exchange is a pool of token-eth trading
 contract UniswapExchange {
     using SafeMath for uint256;
 
@@ -18,27 +19,32 @@ contract UniswapExchange {
     uint256 public constant FEE_RATE = 500;        //fee = 1/feeRate = 0.2%
 
     /// STORAGE
-    uint256 public ethPool;
-    uint256 public tokenPool;
-    uint256 public invariant;
-    uint256 public totalShares;
-    address public tokenAddress;
-    address public factoryAddress;
-    mapping(address => uint256) shares;
+    uint256 public ethPool;             // amount of eth in exchange
+    uint256 public tokenPool;           // amount of token in exchange
+    uint256 public invariant;           // = ethPool * tokenPool
+    uint256 public totalShares;         // 
+    address public tokenAddress;        // token address
+    address public factoryAddress;      // factory address which register this exchange
+    mapping(address => uint256) shares; // 
     ERC20Interface token;
     FactoryInterface factory;
 
     /// MODIFIERS
+    // check if this exchange is initialized
     modifier exchangeInitialized() {
         require(invariant > 0 && totalShares > 0);
         _;
     }
 
     /// CONSTRUCTOR
+    // set token & factory
     constructor(address _tokenAddress) public {
+        // set token
         tokenAddress = _tokenAddress;
-        factoryAddress = msg.sender;
         token = ERC20Interface(tokenAddress);
+        
+        // set factory
+        factoryAddress = msg.sender;
         factory = FactoryInterface(factoryAddress);
     }
 
@@ -50,18 +56,27 @@ contract UniswapExchange {
 
     /// EXTERNAL FUNCTIONS
     function initializeExchange(uint256 _tokenAmount) external payable {
+        // you can initialize exchange only at first time
         require(invariant == 0 && totalShares == 0);
+        
         // Prevents share cost from being too high or too low - potentially needs work
         require(msg.value >= 10000 && _tokenAmount >= 10000 && msg.value <= 5*10**18);
+        
+        // set exchange values
         ethPool = msg.value;
         tokenPool = _tokenAmount;
         invariant = ethPool.mul(tokenPool);
+        
+        // set share
         shares[msg.sender] = 1000;
         totalShares = 1000;
+        
+        // send token to exchange
         require(token.transferFrom(msg.sender, address(this), _tokenAmount));
     }
 
     // Buyer swaps ETH for Tokens
+    // = Payer pays in ETH, payer receives Tokens
     function ethToTokenSwap(
         uint256 _minTokens,
         uint256 _timeout
@@ -69,7 +84,10 @@ contract UniswapExchange {
         external
         payable
     {
+        // need to meet some conditions
         require(msg.value > 0 && _minTokens > 0 && now < _timeout);
+        
+        // get eth from sender & give token to sender
         ethToToken(msg.sender, msg.sender, msg.value,  _minTokens);
     }
 
@@ -82,12 +100,18 @@ contract UniswapExchange {
         external
         payable
     {
+        // need to meet some conditions
         require(msg.value > 0 && _minTokens > 0 && now < _timeout);
+        
+        // do not give bad recipient
         require(_recipient != address(0) && _recipient != address(this));
+        
+        // get eth from sender & give token to recipient
         ethToToken(msg.sender, _recipient, msg.value,  _minTokens);
     }
 
     // Buyer swaps Tokens for ETH
+    // = Payer pays in Tokens, payer receives ETH
     function tokenToEthSwap(
         uint256 _tokenAmount,
         uint256 _minEth,
@@ -95,7 +119,10 @@ contract UniswapExchange {
     )
         external
     {
+        // need to meet some conditions
         require(_tokenAmount > 0 && _minEth > 0 && now < _timeout);
+        
+        // get token from sender & give eth to sender
         tokenToEth(msg.sender, msg.sender, _tokenAmount, _minEth);
     }
 
@@ -108,21 +135,29 @@ contract UniswapExchange {
     )
         external
     {
+        // need to meet some conditions
         require(_tokenAmount > 0 && _minEth > 0 && now < _timeout);
+        
+        // do not give bad recipient
         require(_recipient != address(0) && _recipient != address(this));
+        
+        // get token from sender & give eth to recipient
         tokenToEth(msg.sender, _recipient, _tokenAmount, _minEth);
     }
 
     // Buyer swaps Tokens in current exchange for Tokens of provided address
     function tokenToTokenSwap(
-        address _tokenPurchased,                  // Must be a token with an attached Uniswap exchange
-        uint256 _tokensSold,
-        uint256 _minTokensReceived,
+        address _tokenPurchased,        // Must be a token with an attached Uniswap exchange (Buyer buyes this token)
+        uint256 _tokensSold,            // buyer gives this amont of token to this exchange
+        uint256 _minTokensReceived,     // buyer wants to get at least this amount of token
         uint256 _timeout
     )
         external
     {
+        // need to meet some conditions
         require(_tokensSold > 0 && _minTokensReceived > 0 && now < _timeout);
+        
+        // get token from sender & give other token to sender
         tokenToTokenOut(_tokenPurchased, msg.sender, msg.sender, _tokensSold, _minTokensReceived);
     }
 
@@ -136,8 +171,13 @@ contract UniswapExchange {
     )
         external
     {
+        // need to meet some conditions
         require(_tokensSold > 0 && _minTokensReceived > 0 && now < _timeout);
+        
+        // do not give bad recipient
         require(_recipient != address(0) && _recipient != address(this));
+        
+        // get token from sender & give other token to recipient
         tokenToTokenOut(_tokenPurchased, msg.sender, _recipient, _tokensSold, _minTokensReceived);
     }
 
@@ -150,9 +190,14 @@ contract UniswapExchange {
         payable
         returns (bool)
     {
+        // should send ether
         require(msg.value > 0);
+        
+        // this request should be from registered exchange
         address exchangeToken = factory.exchangeToTokenLookup(msg.sender);
         require(exchangeToken != address(0));   // Only a Uniswap exchange can call this function
+        
+        // same as other exchange change his eth to token, and give it to recipient
         ethToToken(msg.sender, _recipient, msg.value, _minTokens);
         return true;
     }
@@ -230,16 +275,27 @@ contract UniswapExchange {
         internal
         exchangeInitialized
     {
+        // get fee
         uint256 fee = ethIn.div(FEE_RATE);
+        
+        // calc new exchange values
         uint256 newEthPool = ethPool.add(ethIn);
         uint256 tempEthPool = newEthPool.sub(fee);
         uint256 newTokenPool = invariant.div(tempEthPool);
+        
+        // calc how much token to send
         uint256 tokensOut = tokenPool.sub(newTokenPool);
         require(tokensOut >= minTokensOut && tokensOut <= tokenPool);
+        
+        // set exchange values
         ethPool = newEthPool;
         tokenPool = newTokenPool;
         invariant = newEthPool.mul(newTokenPool);
+        
+        // event occur
         emit EthToTokenPurchase(buyer, ethIn, tokensOut);
+        
+        // send token to recipient
         require(token.transfer(recipient, tokensOut));
     }
 
@@ -252,45 +308,75 @@ contract UniswapExchange {
         internal
         exchangeInitialized
     {
+        // get fee
         uint256 fee = tokensIn.div(FEE_RATE);
+        
+        // calc new exchange values
         uint256 newTokenPool = tokenPool.add(tokensIn);
         uint256 tempTokenPool = newTokenPool.sub(fee);
         uint256 newEthPool = invariant.div(tempTokenPool);
+        
+        // calc how much eth to send 
         uint256 ethOut = ethPool.sub(newEthPool);
         require(ethOut >= minEthOut && ethOut <= ethPool);
+        
+        // set new exchange values
         tokenPool = newTokenPool;
         ethPool = newEthPool;
         invariant = newEthPool.mul(newTokenPool);
+        
+        // event occur
         emit TokenToEthPurchase(buyer, tokensIn, ethOut);
+        
+        // get token from buyer
         require(token.transferFrom(buyer, address(this), tokensIn));
+        
+        // give eth to repicient
         recipient.transfer(ethOut);
     }
 
     function tokenToTokenOut(
-        address tokenPurchased,
+        address tokenPurchased, // Buyer buyes this token and give it to recipient
         address buyer,
         address recipient,
-        uint256 tokensIn,
+        uint256 tokensIn,       // Buyer payes this amount of token to buy other token
         uint256 minTokensOut
     )
         internal
         exchangeInitialized
     {
+        // need to meet some conditions
         require(tokenPurchased != address(0) && tokenPurchased != address(this));
+        
+        // token should be registered within other exchange
         address payable exchangeAddress = addrToPayable(factory.tokenToExchangeLookup(tokenPurchased));
         require(exchangeAddress != address(0) && exchangeAddress != address(this));
+        
+        // set fee
         uint256 fee = tokensIn.div(FEE_RATE);
+        
+        // calc new exchange values
         uint256 newTokenPool = tokenPool.add(tokensIn);
         uint256 tempTokenPool = newTokenPool.sub(fee);
         uint256 newEthPool = invariant.div(tempTokenPool);
+        
+        // calc how much eth to send to other exchange
         uint256 ethOut = ethPool.sub(newEthPool);
         require(ethOut <= ethPool);
+        
+        // get other exchange
         UniswapExchange exchange = UniswapExchange(exchangeAddress);
         emit TokenToEthPurchase(buyer, tokensIn, ethOut);
+        
+        // set exchange values
         tokenPool = newTokenPool;
         ethPool = newEthPool;
         invariant = newEthPool.mul(newTokenPool);
+        
+        // get token from buyer
         require(token.transferFrom(buyer, address(this), tokensIn));
+        
+        // give other token to recipient from other exchange
         require(exchange.tokenToTokenIn.value(ethOut)(recipient, minTokensOut));
     }
     
