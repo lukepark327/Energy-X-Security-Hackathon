bebb34a1420a136f5858a63cc36059ccacbafdb7
ricegood <yeonjaekoo@gmail.com>
2019-10-24 17:12:16 +0900

    Chart color & label

--- code/uniswap-frontend/src/pages/Dashboard/Chart.js
+++ code/uniswap-frontend/src/pages/Dashboard/Chart.js
@@ -6,25 +6,25 @@ import {
 
 const data = [
   {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    name: '00h', Demand: 4000, Supply: 2400, amt: 2400,
   },
   {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    name: '03h', Demand: 3000, Supply: 1398, amt: 2210,
   },
   {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    name: '06h', Demand: 2000, Supply: 2500, amt: 2290,
   },
   {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    name: '09h', Demand: 2780, Supply: 3908, amt: 2000,
   },
   {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    name: '12h', Demand: 1890, Supply: 4800, amt: 2181,
   },
   {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    name: '15h', Demand: 2390, Supply: 3800, amt: 2500,
   },
   {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    name: '18h', Demand: 3490, Supply: 4300, amt: 2100,
   },
 ];
 
@@ -37,6 +37,10 @@ const ChartWrapper = styled.div`
   align-items: center;
   flex: 1;
   overflow: auto;
+
+  .recharts-legend-item-text {
+    color: ${({ theme }) => theme.chaliceGray};
+  }
 `
 
 export default class Chart extends PureComponent {
@@ -57,8 +61,8 @@ export default class Chart extends PureComponent {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="Supply" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="Demand" stroke="#82ca9d" />
       </LineChart>
       </ResponsiveContainer>
       </ChartWrapper>
